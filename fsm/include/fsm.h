177725a8ce630aeee9fe6479ec002987e19738c7
mikael <muchunumike@gmail.com>
2025-03-18 10:15:27 +0300

    f(active_objects): Add generic hsm files

--- fsm/include/fsm.h
+++ fsm/include/fsm.h
@@ -1,5 +1,50 @@
-#ifndef __FSM_H__
+#ifndef FSM_H_
+#define FSM_H_
+
+#include <cstdint>
+
+/*---------------------------------------------------------------------------*/
+/* Event facilities... */
+
+typedef uint16_t Signal;
+
+enum ReservedSignals {
+    INIT_SIGNAL = 0, /* dispatched to AO before entering event-loop */
+    ENTRY_SIGNAL,
+    EXIT_SIGNAL,
+    USER_SIGNAL, /* first signal available to the users */
+};
+
+class Event {
+public:
+    Signal sig; /* event signal */
+    /* event parameters added in subclasses of Event */
+};
+
+/*---------------------------------------------------------------------------*/
+/* Hierarchical State Machine facilities... */
+
+class HSM;
+
+typedef enum{TRAN_STATUS, HANDLED_STATUS, IGNORED_STATUS, INIT_STATUS, SUPER_STATUS} State;
+
+typedef State(*StateHandler)(Event const * const e);
+
+#define TRAN(target_) (((HSM*)me)->state = (StateHandler)(target_), TRAN_STATUS)
+#define SUPER(super_) (((HSM*)me)->temp = (StateHandler)(super_), SUPER_STATUS)
+
+class HSM {
+public:
+    explicit HSM(StateHandler initial);
+
+    void _init(Event const * const e);
+
+    void _dispatch(Event const * const e);
+
+    static State _top(Event const * const e);
+public:
+    StateHandler _state; /* the "state" variable */
+    StateHandler _temp = {}; /* top state machine variable */
+};
 
 #endif
-
-#include "queue"
