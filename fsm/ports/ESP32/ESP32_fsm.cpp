a60b66791ea86ec926449bdc65697ff8eeaa3326
mikael <muchunumike@gmail.com>
2025-03-18 10:06:20 +0300

    f(active_objects): Rename file to be specific to platform

---
+++ fsm/ports/ESP32/ESP32_fsm.cpp
@@ -0,0 +1,28 @@
+#include "ESP32_fsm.h"
+
+#define QUEUE_LENGTH 10
+
+Active::Active(const StateHandler initial): HSM(initial) {
+}
+
+void Active::_start(const UBaseType_t priority) {
+    _priority = priority;
+    _queue = xQueueCreate(QUEUE_LENGTH, sizeof(Event));
+    if (_queue == nullptr) {
+        printf("Queue creation failed!\n");
+    }
+}
+
+void Active::_post(Event const * const e) const {
+    xQueueSend(_queue, e, portMAX_DELAY);
+}
+
+[[noreturn]] void Active::_event_loop() {
+    Event e = {};
+
+    while (true) {
+        if (xQueueReceive(_queue, &e, portMAX_DELAY)) {
+            HSM::_dispatch(&e);
+        }
+    }
+}
