cbbfab2c9654894be66c4fc1fe46e09abebce4c8
mikael <muchunumike@gmail.com>
2025-03-24 16:44:03 +0300

    f(active_objects): Add other LED colours

--- drivers/include/bsp.h
+++ drivers/include/bsp.h
@@ -1,6 +1,7 @@
 #ifndef __BSP_H__
 #define __BSP_H__
 #include "led.h"
+#include "fsm.h"
 
 namespace BSP
 {
@@ -16,6 +17,18 @@ namespace BSP
 
     void BSP_alternate_LED_off();
 
+    void BSP_blue_led_on();
+
+    void BSP_blue_led_off();
+
+    void BSP_green_led_on();
+
+    void BSP_green_led_off();
+
+    void BSP_red_led_on();
+
+    void BSP_red_led_off();
+
     /**
      * @brief blocking wait in line
      * @param ms no. of milliseconds to wait
@@ -25,6 +38,22 @@ namespace BSP
     LED* get_default_onboard_led();
 
     LED* get_alternate_onboard_led();
+
+    LED* get_blue_led();
+
+    LED* get_green_led();
+
+    LED* get_red_led();
 };
+
+enum  EventSignals {
+    BUTTON_PRESSED_SIG = USER_SIGNAL,
+    BUTTON_RELEASED_SIG,
+    BUTTON2_PRESSED_SIG,
+    BUTTON2_RELEASED_SIG,
+    TIMEOUT_SIG,
+    /* */
+    MAX_SIG
+   };
 
 #endif
