366207fd048210cb9f77b8e2a278b0946aa7c017
mikael <muchunumike@gmail.com>
2025-03-24 19:51:09 +0300

    f(active_objects): Resolve compile errors for STM32 target

--- src/main.cpp
+++ src/main.cpp
@@ -14,19 +14,18 @@ int main()
 #endif
 {
     BSP::BSP_init();
-    BSP::BSP_delay(5000);
-    // BSP::BSP_LED_on();
-
-    timeBomb._start();
+    BSP::BSP_LED_on();
     #ifdef ESP32
+    timeBomb._start();
     xTaskCreate(TimeBomb::event_loop, "TimeBomb task", 2048, &timeBomb, 1, nullptr);
-    #else
-    #endif
+
 
     timeBomb._post(&button_pressed_evt);
 
     if (TimerHandle_t my_timer = xTimerCreate("MyTimer", pdMS_TO_TICKS(TIMER_PERIOD_MS), pdTRUE, nullptr, TimeEvent::tick); my_timer != nullptr) {
         xTimerStart(my_timer, 0);
     }
+    #else
+    #endif
 
 }
